7048f4eb616c4a34ac7f013cb1cc8856fa252580
Matheus silva <teus_26@hotmail.com>
2021-04-05 16:06:21 -0300

    struct e typeof

--- pacman.h
+++ pacman.h
@@ -1,3 +1,14 @@
 void liberamapa();
 void lemapa();
 void alocamapa();
+void move(char direcao);
+int acabou();
+void imprimemapa();
+
+struct mapa {
+    char** matriz;
+    int linhas;
+    int colunas;
+};
+
+typedef struct mapa MAPA;
